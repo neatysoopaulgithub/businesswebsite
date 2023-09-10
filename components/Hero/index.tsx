194f842c81c574a84722bc5f23f45c34f80515c1
Neaty Soopaul <neaty.soopaul@gmail.com>
2023-09-10 11:35:41 +0100

    init

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -15,7 +15,7 @@ const Hero = () => {
                 data-wow-delay=".2s"
               >
                 <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-                Our tested fullstack web application is ready for your business.
+                Our tested fullstack web application is ready for your business. 
                 </h1>
                 <p className="mb-12 text-base font-medium !leading-relaxed text-body-color dark:text-white dark:opacity-90 sm:text-lg md:text-xl">
                 Whether you are displaying physical products, digital products or services, this application has all the essential, pages, components, sections and features for a complete business website, built with Next  13.x and Tailwind CSS.
